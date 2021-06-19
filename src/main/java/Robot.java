d3140095175517bcd97636754fa3f8ca57ab73bf
Patrick Henegan <phenegan03@gmail.com>
2021-06-19 12:27:32 -0400

    moved project to a Maven repository created with the instructions on the Pi4J website

--- src/main/java/Robot.java
+++
@@ -1,9 +0,0 @@
-public class Robot {
-    public static void robotInit(){ //Starts on initialization of robot.
-
-    }
-
-    public static void robotPeriodic(){ //Runs continuously while robot is on.
-
-    }
-}
---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,17 @@
+package org.example;
+public class Robot {
+
+    /**
+     * Runs when the robot is first turns on.
+     */
+    public static void robotInit() {
+
+    }
+
+    /**
+     * Runs continuously while the Robot is on.
+     */
+    public static void robotPeriodic() {
+
+    }
+}
