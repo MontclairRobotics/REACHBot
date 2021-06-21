0dd2412a5ce5da822eeba6c3330b59399855bd5e
Dylan Rafael <dylanrafael05@gmail.com>
2021-06-21 14:29:49 -0400

    fix ns errors

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -7,16 +7,11 @@
 
 ### imports ###
 import sys
+import board
+from adafruit_pca9685 import PCA9685
+from busio import I2C
 from typing import Sequence
 from utils import spMAXThrottleToDuty
-
-import board
-import busio
-import adafruit_pca9685
-
-
-### shared vars ###
-verbose = False
 
 
 ### classes ###
@@ -27,15 +22,13 @@ class StopPwmOutput(BaseException):
 
 
 ### functions ###
-def error(s: str):
+def error(e: BaseException):
     """Send an error"""
-    sys.stdout.write('!' + s)
+    sys.stderr.write('ERROR: ' + str(e))
 
 def debug(s: str):
     """Send a debug message"""
-    if verbose:
-        for line in s.splitlines():
-            sys.stdout.write('"' + line)
+    sys.stderr.write(s)
 
 def parse(ipt: str) -> Sequence[float]:
     """Parse the input from a string and return a sequence of floats."""
@@ -50,16 +43,11 @@ def parse(ipt: str) -> Sequence[float]:
 ### main ###
 def main():
 
-    global verbose
-
     pattern = sys.argv[1]
     channels = [int(c, 16) for c in pattern]
 
-    if len(sys.argv) > 2 and sys.argv[2] == 'v':
-        verbose = True
-
-    i2c = busio.I2C(board.SCL, board.SDA)
-    hat = adafruit_pca9685.PCA9685(i2c)
+    i2c = I2C(board.SCL, board.SDA)
+    hat = PCA9685(i2c)
 
     # standard frequency: 50 Hz; 20 ms; 20_000 us
     hat.frequency = 50
@@ -67,8 +55,6 @@ def main():
     # neutralize motors
     for ch in channels:
         hat.channels[ch].duty_cycle = spMAXThrottleToDuty(0)
-
-    debug("Execution started")
 
     while True:
         cmd = None
@@ -80,9 +66,6 @@ def main():
             inp = sys.stdin.readline()
             cmd = parse(inp)
 
-            # echo input
-            debug(f"Received: {inp}")
-
             # set motor speeds
             for i, t in enumerate(cmd):
                 hat.channels[channels[i]].duty_cycle = spMAXThrottleToDuty(t)
@@ -91,7 +74,7 @@ def main():
 
             if not isinstance(e, StopPwmOutput):
                 # an error has occurred and the program should notify java
-                error()
+                error(e)
 
             # gpio should be stopped here
             break
